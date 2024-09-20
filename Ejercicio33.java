2d7cb7e5050e1af48e7df0c424c26047fa2978a7
RobertoLumbreras <rlumbrerascanal@gmail.com>
2024-09-20 20:41:26 +0200

    deleted files from main directory, relocated to src

--- Ejercicio33.java
+++
@@ -1,7 +0,0 @@
-public class Ejercicio33 {
-    public static void main(String[] args) {
-        String txt = "1234";
-        int i = Integer.parseInt(txt)+10;
-        System.out.println(i);
-    }
-}
--- Ejercicio9.java
+++
@@ -1,9 +0,0 @@
-
-import javax.swing.JOptionPane;
-
-public class Ejercicio9 {
-    public static void main(String[] args) {
-        String s = JOptionPane.showInputDialog("Introduce your user name");
-        JOptionPane.showMessageDialog(null, "Welcome back, "+s);
-    }
-}
